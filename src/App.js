dba28a8acb7009a5208aa8884a2ff47c635e9613
lo <lo@example.com>
2022-03-26 16:41:56 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getUser } from "./data";
 
 function App() {
@@ -17,6 +17,7 @@ function Header(props){
   const user = props.user;
   var colspan = user.userClass === "Student" ? 3 : 0;
   const courseName = "Asignatura 1";
+  let navigate = useNavigate();
 
   return (
     <table width="100%" border="1" className="reference">
@@ -56,9 +57,12 @@ function Header(props){
           </table>
         </td>
         <td width="9%">
-          <nav>
-            <Link to="/logout">Desconectar</Link>
-          </nav>
+          <button className="button" type="logout" onClick={() => { navigate("/logout")}}>
+            <div className="divinline">
+             Logout
+            <svg  viewBox="0 0 448 512"><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg>
+            </div>
+          </button>        
         </td>
       </tr>
     </tbody>
--- src/routes/Pagina.js
+++ src/routes/Pagina.js
@@ -1,18 +1,30 @@
 import {useNavigate} from "react-router-dom";
+import { useState } from "react";
 
 export default function Pagina() {
     let navigate = useNavigate();
+    const [userName, setUserName] = useState("Usuario");
+    const [userPass, setUserPass] = useState("Clave");
     return (
         <div className="fondo">
-            <h1 align="center">&nbsp;</h1>
-            <h1 align="center" className="title">Informes de Trabajo en Grupo. Autenticaci&oacute;n</h1>
-            <label>Usuario:<input value ="usuario" name="tfNombreUsuario"/></label>
-            <label>Clave:<input value ="clave" name="tfClave" /></label>
-            <p></p>
+            <h1 align="center" className="title">Autenticaci&oacute;n</h1>
+            <h2 align="center" className="title">Informes de Trabajo en Grupo</h2>
+            <br/>
+            <br/>            
+            <label>Usuario:<input value ={userName} onChange={event => setUserName(event.target.value)}/></label>
+            <br/>
+            <br/>
+            <label>Clave:<input  type="password" name="password" value ={userPass} onChange={event => setUserPass(event.target.value)} /></label>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>            
             <button className="button" type="submit" onClick={() => { navigate("/")}}>
                 Login
             </button>
-        </div>
+        </div>      
     );
   }
   
