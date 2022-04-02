35d372499cc78b4f8f2cc496c3286980150e6e72
lo <lo@example.com>
2022-04-02 14:11:39 +0200

    bootstrap2

--- src/Login.js
+++ src/Login.js
@@ -1,54 +1,82 @@
+
+import { Button, Form, Row, Col, Container,Stack } from 'react-bootstrap';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faAngry } from '@fortawesome/free-regular-svg-icons';
+import 'App.css';
 import {useNavigate} from "react-router-dom";
-import { useState } from "react";
 import { authenticate } from "./app/data";
 
 
-export default function Login() {
-    let navigate = useNavigate();
-    const [userName, setUserName] = useState("Usuario");
-    const [userPass, setUserPass] = useState("Clave");
+function Loging() {
 
-    function submit (event) {
-        event.preventDefault();        
-        //alert('A user and password were submitted: '+userName+" "+userPass);
-        var authUser = authenticate(userName,userPass);
-        if (authUser===undefined)
-            alert ("No existe el usuario o la clave es incorrecta");
-        else{
-            sessionStorage.setItem('localUser',JSON.stringify(authUser));
-            navigate("/app");
-        }
+  let navigate = useNavigate();
+  const [userName, setUserName] = useState("Usuario");
+  const [userPass, setUserPass] = useState("Clave");
 
+  function submit (event) {
+      event.preventDefault();        
+      //alert('A user and password were submitted: '+userName+" "+userPass);
+      var authUser = authenticate(userName,userPass);
+      if (authUser===undefined)
+          alert ("No existe el usuario o la clave es incorrecta");
+      else{
+          sessionStorage.setItem('localUser',JSON.stringify(authUser));
+          navigate("/app");
       }
-      
 
-    return (
-        <div className="fondo">
-            <form type="submit" /*method="post"*/ onSubmit={submit}>
-                <h1 align="center" className="title">Autenticaci&oacute;n</h1>
-                <h2 align="center" className="title">Informes de Trabajo en Grupo</h2>
-                <br/>
-                <br/>            
-                <label>
-                    Usuario:
-                    <input name="user" value ={userName} onChange={event => setUserName(event.target.value)}/>
-                </label>
-                <br/>
-                <br/>
-                <label>
-                    Clave:
-                    <input  type="password" name="password" value ={userPass} onChange={event => setUserPass(event.target.value)} />
-                </label>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>            
-                <button className="button" type="submit" value="submit">
-                    Login
-                </button>
-            </form>
-        </div>      
-    );
-  }
-  
+    }
+
+/*d-flex justify-content-center*/
+const longw = {width: '400px'}
+  return (
+    <>
+    <div className="m-4">
+        <h1 className="text-primary text-center">Autenticación</h1>
+        <h2 className="text-primary text-center">Informes de Trabajo en Grupo</h2>
+        <i className="fa fa-eye" aria-hidden="true"></i>
+    </div>      
+    <div className="mx-auto" style={longw}>  
+      <Form  onSubmit={submit}>
+        <br></br>
+        <Container className="p-3 bg-light border border-primary rounded">
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formUser">
+                <Form.Label>Usuario:</Form.Label>
+                <Form.Control type="text" placeholder="usuario" onChange={event => setUserName(event.target.value)}/>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Clave:</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={event => setUserPass(event.target.value)} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="row justify-content-center">
+              <Button className="w-50 mb-3" variant="primary" type="submit">
+                Login <FontAwesomeIcon icon={faAngry} />    
+              </Button>
+            </Col>
+          </Row>
+        </Container>      
+      </Form>
+    </div>
+
+
+<FontAwesomeIcon icon={solid('user-secret')} />
+<FontAwesomeIcon icon={faAngry} />                
+<FontAwesomeIcon icon={regular('angry')} />
+<FontAwesomeIcon icon={brands('twitter')} />
+
+    </>    
+  );
+}
+
+export default Login;
