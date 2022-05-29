54f27e0d0e8dae31c1dbc8bf034a64b3b513f20b
lo <lo@example.com>
2022-05-29 18:35:13 +0200

    fetch api

--- src/app/apiCalls/api.js
+++ src/app/apiCalls/api.js
@@ -1,34 +1,56 @@
 const baseURL = process.env.REACT_APP_API_URL;
 const abortTime = process.env.REACT_APP_API_TIMEOUT;
 
-async function checkError (response){
-    if (!response.ok) throw new Error(response.statusText);
-    return response;
-}
-
 function abortTimeout(){
     let controller = new AbortController();
     let signal = controller.signal;
     setTimeout(() => controller.abort(), abortTime);
     return signal;
 }
-/*
-export async function findAllDegrees ()  {
-    let signal = abortTimeout();
-    return fetch(baseURL+"/degrees",{signal}).then(checkError);
- }
- */
 
- export async function fetchGet (url){
-    let signal = abortTimeout(); 
+async function checkError (response){
     return await new Promise ((resolve,reject)=>{
-        fetch(baseURL+url,{signal})
-            .then(checkError)
-            .then(response=>{resolve(response.json())})
-            .catch(error=>{reject(error)});
-    })    
+        if (response.ok){
+            resolve(response.json());
+        }else{
+            reject(response.text());
+        }       
+    }
+    )
+}
+
+async function fetchGet (url){
+    let signal = abortTimeout(); 
+    return fetch(baseURL+url,{signal}).then(checkError)
+}
+
+async function fetchPost (url, data){
+    //let signal = abortTimeout(); data can be saved despite abort signal was sent
+    const options = {
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json", 
+          //"Accept":"application/json",
+          //"mode":"no-cors",
+        },
+        body:JSON.stringify(data)//, signal:signal
+    };
+
+    return  fetch(baseURL+url,options).then(checkError)
 }
 
 export async function findAllDegrees ()  {
-    return fetchGet("/degrees");
+    return fetchGet("/degree/degrees");
+}
+
+export async function saveDegree (data)  {
+    return fetchPost("/degree/degree",data);
+}
+
+export async function countByDegreeName (data)  {
+    return fetchGet("/degree/countbyname/"+data);
+}
+
+export async function existsByDegreeName (data)  {
+    return fetchGet("/degree/existsbyname/"+data);
 }
--- src/app/components/DegreesTable.js
+++ src/app/components/DegreesTable.js
@@ -1,11 +1,14 @@
-import {Table, Spinner, Alert, Button} from 'react-bootstrap';
-import { useEffect, useState} from "react";
-import {findAllDegrees, findAllDegrees2} from '../apiCalls/api';
+import { Table, Spinner, Alert, Button, Form } from 'react-bootstrap';
+import { useEffect, useState } from "react";
+import { findAllDegrees, saveDegree, existsByDegreeName } from '../apiCalls/api';
 
-function DegreesTable (){
+function DegreesTable() {
     const [isLoading, setIsLoading] = useState(true);
-    const [degrees, setdegrees] = useState([]);
-    const [fetchError, setFetchError] = useState(false);
+    const [degrees, setDegrees] = useState([]);
+    const [fetchError, setFetchError] = useState("");
+    const [validatedForm, setValidatedForm] = useState(false);
+    const [degreeName, setDegreeName] = useState("");
+    const [saveError, setSaveError] = useState("");
 
     /*
     const checkError = response => {
@@ -42,43 +45,101 @@ function DegreesTable (){
     */
 
     const buscarDatos = () => {
-         findAllDegrees()
-             .then((data) => {  
-                 setdegrees(data);
-                 setIsLoading(false);
-                 console.log(degrees);
-             })
-             .catch((error)=> {
-                 console.log(error.name);
-                 console.log(error);
-                 setFetchError(true);
-                 setIsLoading(false);
-             });            
-     };
-      
+        findAllDegrees()
+            .then((data) => {
+                setDegrees(data);
+                setIsLoading(false);
+                console.log(degrees);
+            })
+            .catch((err) => {
+                Promise.resolve(err).then(err=>{setFetchError(err.toString())})
+                setIsLoading(false);
+            });
+    };
+
 
     useEffect(() => {
         buscarDatos();
-      }, []);
+    }, []);
 
-    
-//    console.log("b:"+degrees)
+    function save(data){
+        saveDegree(data)
+        .then(
+            function(res){ 
+                setDegrees([...degrees,res ])
+                setDegreeName("")
+                setSaveError("")
+            },
+            function(err) {
+                //Promise.resolve(err) 'cause err can be a Promise or not
+                Promise.resolve(err).then(err=>{setSaveError(err.toString())})
+            }
+        )
+    }
+
+    function test(data){
+        existsByDegreeName(data)
+        .then(
+            function(res){ 
+                if(!res){
+                    save({/*"id":1,*/"name":degreeName.trim()})  
+                    setValidatedForm(false)
+                }
+                else
+                    setSaveError("La asignatura ya existe")
+            },      
+            function(err) {
+                //Promise.resolve(err) 'cause err can be a Promise or not
+                Promise.resolve(err).then(err=>{setSaveError(err.toString())})
+            }
+        )
+    }
+
+    function submit(e) {
+        e.preventDefault();
+        setValidatedForm(true);
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+        else{            
+            //save({/*"id":1,*/"name":degreeName.trim()})        
+            test(degreeName.trim())        
+        }                
+    }
+
+
+    //    console.log("b:"+degrees)
 
     return (
         <>
-        <Table>
-            <thead>
-                <tr><th>Id</th><th>Degree Title</th></tr>
-            </thead>
-            {!isLoading&&!fetchError&&
-            <tbody>
-                {degrees.map(degree=>{return(<tr key={degree.id}><td>{degree.id}</td><td>{degree.name}</td></tr>) })}
-            </tbody>
-            }
-        </Table>
-        {fetchError&&<Alert variant="danger">Error al recuperar los datos ...</Alert>}
-        {isLoading&&<div>...cargando &nbsp; <Spinner animation="border" variant="primary" /></div>}
-        {/*isLoading&&<Button onClick={buscarDatos}>cargar</Button>*/}
+            <Table>
+                <thead>
+                    <tr><th>Id</th><th>Degree Title</th></tr>
+                </thead>
+                {!isLoading && !fetchError &&
+                    <tbody>
+                        {degrees.map(degree => { return (<tr key={degree.id}><td>{degree.id}</td><td>{degree.name}</td></tr>) })}
+                    </tbody>
+                }
+            </Table>
+            {fetchError!=="" && <Alert variant="danger">Error al recuperar los datos ...{fetchError}</Alert>}
+            {isLoading && <div>...cargando &nbsp; <Spinner animation="border" variant="primary" /></div>}
+            {/*isLoading&&<Button onClick={buscarDatos}>cargar</Button>*/}
+            <Form noValidate validated={validatedForm} onSubmit={submit} id="degreeForm">
+                <Form.Group className="mb-3">
+                    <Form.Label>Nombre Asignatura</Form.Label>
+                    <Form.Control type="text" maxLength={50} minLength={2} required placeholder="Teclea el nombre"
+                                  value ={degreeName} onChange={(e)=>setDegreeName(e.target.value)} />
+                    <Form.Text className="text-muted">
+                        Mínimo 2 caracteres y Máximo 50.
+                    </Form.Text>
+                    <Form.Control.Feedback type="invalid">Debe escribir un nombre de asignatura con al menos 2 caracteres</Form.Control.Feedback>
+                </Form.Group>
+                {saveError!==""&&<Alert  variant="danger">Error, no se ha podido guardar la asignatura: {saveError}</Alert>}                
+                <Button type="submit" form="degreeForm">añadir</Button>
+            </Form>
+
         </>
     )
 
